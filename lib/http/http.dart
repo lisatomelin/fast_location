0a4d5b3375b2b20337decca34a2a4bb1c2ae8651
liviabarcelos <liviahbarcellos@gmail.com>
2025-05-01 15:32:17 -0300

    add:http

---
+++ lib/http/http.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import '../../modules/home/model/address_model.dart';
+
+class ApiCall extends StatelessWidget {
+  const ApiCall({super.key});
+
+  Future<List<AddressModel>> fetchAPI(cep) async {
+    Dio dio = Dio();
+
+    var response = await dio.get('https://viacep.com.br/ws/$cep/json/');
+
+    final List<dynamic> responseData = response.data;
+    List<AddressModel> result =
+        responseData.map((json) => AddressModel.fromJson(json)).toList();
+
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
