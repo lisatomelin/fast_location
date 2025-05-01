52a33d3f7066433d7a47dbc848a82c0737f37278
João Vitor Matteucci Reiter <60893997+JoaoReiter@users.noreply.github.com>
2025-05-01 16:17:03 -0300

    :sparkles:Feat Adicionado search em components

---
+++ lib/modules/home/components/search_address.dart
@@ -0,0 +1,116 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:fast_location/src/modules/home/model/address_model.dart';
+
+class SearchAddress extends StatefulWidget {
+  final AddressModel address;
+
+  const SearchAddress({
+    Key? key,
+    required this.address,
+  }) : super(key: key);
+
+  @override
+  State<SearchAddress> createState() => _SearchAddressState();
+}
+
+class _SearchAddressState extends State<SearchAddress> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                "Dados da Localização",
+                style: TextStyle(
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18),
+              )
+            ],
+          ),
+          const SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Text(
+                "Logradouro/Rua: ",
+                style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(widget.address.logradouro)
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Text(
+                "Bairro/Distrito: ",
+                style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(widget.address.bairro)
+            ],
+          ),
+          if (widget.address.complemento != '')
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Complemento: ",
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(widget.address.complemento)
+                ],
+              ),
+            ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Text(
+                "Cidade/UF: ",
+                style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text('${widget.address.logradouro}/${widget.address.uf}')
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Text(
+                "CEP: ",
+                style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(widget.address.cep)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
