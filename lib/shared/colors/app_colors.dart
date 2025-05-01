e957e580393b800a1cf17fc73d03bfb2a8b27110
Lisi Tomelin <lisa_tomelin@hotmail.com>
2025-04-30 21:20:11 -0300

    add: incluindo app_colors

---
+++ lib/shared/colors/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color appPageBackground = Color(0xFFE7E7E7);
+  static const Color appBarBackground = Colors.green;
+  static const Color appBarContainer = Color(0xffd5d4d4);
+}
